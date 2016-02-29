74ce8dcb2e409294912f8c3a3dcc0b45c24890b1
Eugene Samoylov <e.samoylov@2Gis.ru>
2016-02-29 23:13:31 +0600

    add uart, 328P stuff and bootloader

--- main.c
+++ main.c
@@ -6,9 +6,12 @@
 //#include <avr/sleep.h>
 //#include <avr/pgmspace.h>
 //#include <string.h>
+#include "uart.h"
 
 #define FALSE         0
 #define TRUE          1
+
+#define LED           5
 
 #define TGLBIT(REG, BIT)   (REG ^= (1 << BIT))
 #define CLRBIT(REG, BIT)   (REG &= ~(1 << BIT))
@@ -17,13 +20,15 @@
 
 int main(void) 
 {
-// configure PD0 for output and blink
-	DDRD = 1;
-	PORTD = 1;
+	SETBIT(DDRB, LED);
+	SETBIT(PORTB,LED);
+
+	uart_init();
 
 	while (1) {
 		_delay_ms(500);		
-		TGLBIT(PORTD,0);
+		TGLBIT(PORTB,LED);
+		printf("Hellow!\n\r");
 	}
 	return 0;
 }
---
+++ uart.h
@@ -0,0 +1,10 @@
+#ifndef _uart_h
+#define _uart_h 1
+
+#include <stdio.h>
+
+#define BAUDRATE 57600     //baudrate (18n)
+
+void uart_init(void);
+
+#endif /* defined _uart_h */
